163bca45f665173ea87d0442377b58ef2ddb87e6
Seth <58447309+ohmygaugh-crypto@users.noreply.github.com>
2023-10-12 13:25:55 -0400

    added manual install button 4 testing only

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
@@ -7,12 +7,46 @@ import ShareButton from './components/ShareButton';
 import ShareTargetComponent from './components/ShareTargetComponent'; // Importing the ShareTargetComponent
 
 function App() {
+
+  const [deferredPrompt, setDeferredPrompt] = useState<any>(null);
+  const [showInstallButton, setShowInstallButton] = useState(false);
+
+  useEffect(() => {
+    function handleInstallPrompt(e: any) {
+      e.preventDefault();
+      setDeferredPrompt(e);
+      setShowInstallButton(true); // Show the install button when the event is fired
+    }
+
+    window.addEventListener('beforeinstallprompt', handleInstallPrompt);
+
+    return () => {
+      window.removeEventListener('beforeinstallprompt', handleInstallPrompt);
+    };
+  }, []);
+
+  const handleInstallClick = () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+      deferredPrompt.userChoice.then((choiceResult: any) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        setDeferredPrompt(null);
+        setShowInstallButton(false); // Hide the button after the prompt is resolved
+      });
+    }
+  };
+
   return (
     <Router>
       <div className="App">
         <header className="App-header">
           <SearchBar />
           <ShareButton />
+          {showInstallButton && <button onClick={handleInstallClick}>Install App</button>}
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
