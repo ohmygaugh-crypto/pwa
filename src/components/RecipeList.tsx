7242b3b1ac95da342632f8bcd4177b6a7e3029d3
Seth <58447309+ohmygaugh-crypto@users.noreply.github.com>
2023-10-30 14:30:09 -0400

    updated reactmagicmotion and fixed Navigate Hook

--- src/components/RecipeList.tsx
+++ src/components/RecipeList.tsx
@@ -1,56 +1,54 @@
 import React, { useEffect, useState } from 'react';
 import { MagicMotion } from 'react-magic-motion';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, NavigateFunction } from 'react-router-dom';
 
+interface RecipeListItemProps {
+  title: string;
+  handleDelete: (title: string) => void;
+  navigate: NavigateFunction;
+}
 
-function RecipeListItem({ title, handleDelete }: { title: string; handleDelete: (title: string) => void }) {
-    const navigate = useNavigate(); // <-- Use the useHistory hook
+function RecipeListItem({ title, handleDelete, navigate }: RecipeListItemProps) {
+  const navigateToRecipe = () => {
+    navigate(`/results?recipe=${encodeURIComponent(localStorage.getItem(title) || '')}`);
+  };
 
-    const navigateToRecipe = () => {
-        navigate(`/results?recipe=${encodeURIComponent(localStorage.getItem(title) || '')}`);
-    };
-    
-    
-    return (
-        <li>
-            <div onClick={navigateToRecipe} style={{ cursor: 'pointer' }}> {/* <-- Add the onClick event here */}
-                {title}
-            </div>
-            <button onClick={() => handleDelete(title)}>Delete</button>
-        </li>
-    );
+  return (
+    <li>
+      <div onClick={navigateToRecipe} style={{ cursor: 'pointer' }}>
+        {title}
+      </div>
+      <button onClick={() => handleDelete(title)}>Delete</button>
+    </li>
+  );
 }
 
 function RecipeList() {
-    const [recipeTitles, setRecipeTitles] = useState<string[]>([]);
+  const [recipeTitles, setRecipeTitles] = useState<string[]>([]);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const keys = Object.keys(localStorage);
-        const titles = keys.filter(key => key !== 'loglevel:webpack-dev-server');
-        setRecipeTitles(titles);
-    }, []);
-    
-    const handleDelete = (titleToDelete: string) => {
-        localStorage.removeItem(titleToDelete);
-        //const keys = Object.keys(localStorage); {/* commented out to fix netlify build typescript error*/}
-        //const titles = keys.filter(key => key !== 'loglevel:webpack-dev-server');  {/* commented out to fix netlify build typescript error*/}
-        setRecipeTitles(prevTitles => prevTitles.filter(title => title !== titleToDelete));
-    };
+  useEffect(() => {
+    const keys = Object.keys(localStorage);
+    const titles = keys.filter((key) => key !== 'loglevel:webpack-dev-server');
+    setRecipeTitles(titles);
+  }, []);
 
-    return (
-        <MagicMotion>
-            <div style={{ display: "flex", flexDirection: "column", gap: "1.5rem", marginTop: "1rem" }}>
-                <ul style={{ display: "flex", flexDirection: "column", gap: "0.75rem", overflow: "hidden" }}>
-                    {recipeTitles.map(title => (
-                        <RecipeListItem key={title} title={title} handleDelete={handleDelete} />
-                    ))}
-                </ul>
-            </div>
-        </MagicMotion>
-    );
+  const handleDelete = (titleToDelete: string) => {
+    localStorage.removeItem(titleToDelete);
+    setRecipeTitles((prevTitles) => prevTitles.filter((title) => title !== titleToDelete));
+  };
+
+  return (
+    <MagicMotion>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '1.5rem', marginTop: '1rem' }}>
+        <ul style={{ display: 'flex', flexDirection: 'column', gap: '0.75rem', overflow: 'hidden' }}>
+          {recipeTitles.map((title) => (
+            <RecipeListItem key={title} title={title} handleDelete={handleDelete} navigate={navigate} />
+          ))}
+        </ul>
+      </div>
+    </MagicMotion>
+  );
 }
 
 export default RecipeList;
-
-
-//clicking a instruction step on this page highlights the contents in a green box as well as highlighting the measurements within the ingredients section!
